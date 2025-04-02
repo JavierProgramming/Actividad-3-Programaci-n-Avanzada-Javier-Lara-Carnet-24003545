3f394c3f3f81f57aa36cfd2c926bd3c7f0f279eb
JavierProgramming <158225079+JavierProgramming@users.noreply.github.com>
2025-04-02 17:04:39 -0600

    Create ProgressBar.js

---
+++ components/ProgressBar.js
@@ -0,0 +1,7 @@
+export default function ProgressBar() {
+  return (
+    <div className="w-full bg-gray-300 rounded-full h-4">
+      <div className="bg-red-500 h-4 w-1/3 rounded-full"></div>
+    </div>
+  );
+}
